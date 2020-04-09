aa93cadd71beb4e24e7731a78bc7f30e081dda30
86159 <shichaoran@vandream.com>
2020-04-09 13:35:41 +0800

    更新

--- di-analyze-api/src/main/java/com/vd/canary/data/api/feign/es/ShopServeiceFallbackFactory.java
+++ di-analyze-api/src/main/java/com/vd/canary/data/api/feign/es/ShopServeiceFallbackFactory.java
@@ -3,6 +3,7 @@ package com.vd.canary.data.api.feign.es;
 import com.vd.canary.core.bo.ResponseBO;
 import com.vd.canary.core.exception.BusinessException;
 import com.vd.canary.data.api.request.es.ShopPageBO;
+import com.vd.canary.data.api.request.es.ShopSearchBO;
 import feign.hystrix.FallbackFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -24,17 +25,23 @@ public class ShopServeiceFallbackFactory implements FallbackFactory<ShopServeice
     public ShopServeiceFeign create(Throwable e) {
         return new ShopServeiceFeign() {
 
+            /**
+             * 商铺搜索
+             *
+             * @param shopSearchBO
+             */
             @Override
-            public ResponseBO<ShopPageBO> search(@Valid ShopPageBO shopPageBO) {
+            public ResponseBO<ShopPageBO> search(@Valid ShopSearchBO shopSearchBO) {
                 return null;
             }
+
             /**
              * 商铺详情
              *
              * @param shopPageBO
              */
             @Override
-            public ResponseBO<ShopPageBO> getByKey(@Valid ShopPageBO shopPageBO) {
+            public ResponseBO<ShopPageBO> getByID(@Valid ShopPageBO shopPageBO) {
                 return null;
             }
         };
--- di-analyze-api/src/main/java/com/vd/canary/data/api/feign/es/ShopServeiceFeign.java
+++ di-analyze-api/src/main/java/com/vd/canary/data/api/feign/es/ShopServeiceFeign.java
@@ -1,14 +1,11 @@
 package com.vd.canary.data.api.feign.es;
 
-import com.sun.istack.NotNull;
 import com.vd.canary.core.api.Feign;
 import com.vd.canary.core.bo.ResponseBO;
 import com.vd.canary.data.api.request.es.ShopPageBO;
-import com.vd.canary.data.api.response.es.ESEntityWordRes;
-import com.vd.canary.data.api.response.es.vo.Keyword;
+import com.vd.canary.data.api.request.es.ShopSearchBO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -28,7 +25,7 @@ public interface ShopServeiceFeign extends Feign {
      *商铺搜索
      */
     @PostMapping("/shop/search")
-    ResponseBO<ShopPageBO> search(@RequestBody @Valid  Keyword keyword);
+    ResponseBO<ShopPageBO> search(@RequestBody @Valid ShopSearchBO shopSearchBO);
     /**
      * 商铺详情
      */
