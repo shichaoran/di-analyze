c9db73bc36659ba633b42c8c911d125fc33cb0c3
wangruilin9339 <1464791172@qq.com>
2020-04-09 11:31:19 +0800

    合并商品feign

---
+++ di-analyze-api/src/main/java/com/vd/canary/data/api/feign/es/ProductsServiceFeignFallback.java
@@ -0,0 +1,46 @@
+package com.vd.canary.data.api.feign.es;
+
+import com.vd.canary.core.bo.ResponseBO;
+import com.vd.canary.core.bo.ResponsePageBO;
+import com.vd.canary.core.exception.BusinessException;
+import com.vd.canary.data.api.request.es.CategoryReq;
+import com.vd.canary.data.api.request.es.ProductDetailsReq;
+import com.vd.canary.data.api.request.es.ProductsReq;
+import com.vd.canary.data.api.response.es.CategoryRes;
+import com.vd.canary.data.api.response.es.ProductDetailsRes;
+import com.vd.canary.data.api.response.es.ProductsRes;
+import feign.hystrix.FallbackFactory;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import static com.vd.canary.core.constant.HttpResponseStatus.FEIGN_EXCEPTION;
+
+public class ProductsServiceFeignFallback implements FallbackFactory<ProductsServiceFeign> {
+
+
+    @Override
+    public ProductsServiceFeign create(Throwable e) {
+        return new ProductsServiceFeign() {
+            @Override
+            public ResponsePageBO<ProductsRes> getProductsByKey(@Valid ProductsReq productsReq) {
+                throw new BusinessException(FEIGN_EXCEPTION).append(e.getMessage());
+            }
+
+            @Override
+            public ResponseBO<CategoryRes> categoryres(@Valid CategoryReq categoryReq) {
+                throw new BusinessException(FEIGN_EXCEPTION).append(e.getMessage());
+            }
+
+            @Override
+            public ResponseBO<ProductDetailsRes> get(@NotNull String id) {
+                throw new BusinessException(FEIGN_EXCEPTION).append(e.getMessage());
+            }
+
+            @Override
+            public ResponseBO<ProductDetailsRes> getProductsDetail(@Valid ProductDetailsReq productDetailsReq) {
+                throw new BusinessException(FEIGN_EXCEPTION).append(e.getMessage());
+            }
+        };
+    }
+}
