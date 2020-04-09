d982c965028ddddd9ed7eb31fe6603cced9d8c84
wangruilin9339 <1464791172@qq.com>
2020-04-09 10:11:31 +0800

    合并商品feign

--- di-analyze-api/src/main/java/com/vd/canary/data/api/feign/es/ProductsFeign.java
+++ di-analyze-api/src/main/java/com/vd/canary/data/api/feign/es/ProductsFeign.java
@@ -1,27 +1,44 @@
 package com.vd.canary.data.api.feign.es;
 
 import com.vd.canary.core.api.Feign;
+import com.vd.canary.core.bo.ResponseBO;
 import com.vd.canary.core.bo.ResponsePageBO;
+import com.vd.canary.data.api.request.es.CategoryReq;
+import com.vd.canary.data.api.request.es.ProductDetailsReq;
 import com.vd.canary.data.api.request.es.ProductsReq;
+import com.vd.canary.data.api.response.es.CategoryRes;
+import com.vd.canary.data.api.response.es.ProductDetailsRes;
 import com.vd.canary.data.api.response.es.ProductsRes;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @FeignClient(value = "canary-dataanalyze",
         contextId = "productsFeign",
         decode404 = true,
         fallbackFactory = ProductsFeignFallback.class)
+
+
 public interface ProductsFeign extends Feign {
     /**
      *
      * @param productsReq
      * @return ResponsePageBO<ProductsRes>
      */
-    @PostMapping("/es/getProductsByKey")
+    @PostMapping("/products/getProductsByKey")
     ResponsePageBO<ProductsRes> getProductsByKey(@RequestBody @Valid ProductsReq productsReq);
 
+    @PostMapping("/products/categoryres")
+    ResponseBO<CategoryRes> categoryres(@RequestBody @Valid CategoryReq categoryReq);
+
+    @GetMapping("/products/getProductsById")
+    ResponseBO<ProductDetailsRes> get(@PathVariable("id") @NotNull String id);
+    @PostMapping("/products/getProductsDetail")
+    ResponseBO<ProductDetailsRes> getProductsDetail(@RequestBody @Valid ProductDetailsReq productDetailsReq);
 
 }
--- di-analyze-api/src/main/java/com/vd/canary/data/api/feign/es/ProductsFeignFallback.java
+++ di-analyze-api/src/main/java/com/vd/canary/data/api/feign/es/ProductsFeignFallback.java
@@ -1,12 +1,18 @@
 package com.vd.canary.data.api.feign.es;
 
+import com.vd.canary.core.bo.ResponseBO;
 import com.vd.canary.core.bo.ResponsePageBO;
 import com.vd.canary.core.exception.BusinessException;
+import com.vd.canary.data.api.request.es.CategoryReq;
+import com.vd.canary.data.api.request.es.ProductDetailsReq;
 import com.vd.canary.data.api.request.es.ProductsReq;
+import com.vd.canary.data.api.response.es.CategoryRes;
+import com.vd.canary.data.api.response.es.ProductDetailsRes;
 import com.vd.canary.data.api.response.es.ProductsRes;
 import feign.hystrix.FallbackFactory;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import static com.vd.canary.core.constant.HttpResponseStatus.FEIGN_EXCEPTION;
 
@@ -18,6 +24,21 @@ public class ProductsFeignFallback implements FallbackFactory<ProductsFeign> {
         return new ProductsFeign() {
             @Override
             public ResponsePageBO<ProductsRes> getProductsByKey(@Valid ProductsReq productsReq) {
+                throw new BusinessException(FEIGN_EXCEPTION).append(e.getMessage());
+            }
+
+            @Override
+            public ResponseBO<CategoryRes> categoryres(@Valid CategoryReq categoryReq) {
+                throw new BusinessException(FEIGN_EXCEPTION).append(e.getMessage());
+            }
+
+            @Override
+            public ResponseBO<ProductDetailsRes> get(@NotNull String id) {
+                throw new BusinessException(FEIGN_EXCEPTION).append(e.getMessage());
+            }
+
+            @Override
+            public ResponseBO<ProductDetailsRes> getProductsDetail(@Valid ProductDetailsReq productDetailsReq) {
                 throw new BusinessException(FEIGN_EXCEPTION).append(e.getMessage());
             }
         };
