fbf51fe1e0b9c23a81c114264d11b6e93ba48ac4
luojianbing <luojb@ngarihealth.com>
2020-04-17 15:28:15 +0800

    upgrade version

--- di-analyze-service/src/main/java/com/vd/canary/data/DataAnalyzeApplication.java
+++ di-analyze-service/src/main/java/com/vd/canary/data/DataAnalyzeApplication.java
@@ -5,7 +5,10 @@ import com.vd.canary.service.constants.ClusterConstant;
 import com.vd.canary.service.launch.CanaryApplication;
 
 
-@CanaryCloudApplication(clusterName = ClusterConstant.OBMP, applicationName = "canary-dataanalyze",nacosNamespaceId="13c80def-b092-4d3f-b662-64e2ecdf2d6c")
+//@CanaryCloudApplication(clusterName = ClusterConstant.OBMP, applicationName = "canary-dataanalyze",nacosNamespaceId="13c80def-b092"
+//                                                                                                                     + "-4d3f-b662"
+//        + "-64e2ecdf2d6c")
+@CanaryCloudApplication(clusterName = ClusterConstant.OBMP, applicationName = "canary-dataanalyze")
 public class DataAnalyzeApplication {
     //System.setProperty("es.set.netty.runtime.available.processors","false");
     public static void main(String[] args) {
